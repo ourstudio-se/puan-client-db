2b4825571160121d3811a6970b370cdf105f011e
Rikard Olsson <rikard@thryselius.se>
2023-01-19 21:26:54 +0100

    Basic features only implemented

--- puan_client_db/__init__.py
+++ puan_client_db/__init__.py
@@ -3,76 +3,65 @@ import puan.logic.plog as pg
 
 from puan import Proposition
 from dataclasses import dataclass
-from typing import Optional
+from typing import Optional, Tuple, Any
 
 @dataclass
 class Client:
 
     url: str
 
-    def init(self, model: Proposition) -> Optional[str]:
+    @staticmethod
+    def _extract_data(response, key) -> Tuple[Optional[Any], Optional[str]]:
 
         """
-            Initializing a new version control monitor on `model`.
+            Extracts data from response by using given callable `key` function.
 
             Returns
             -------
-            bool
+            Tuple[Optional[str], Optional[str]]
+                Left side is Any data, if it was successfully got. Else None.
+                Right side is an error string, if something went wrong. Else None.
         """
-        response = requests.post(f"{self.url}/model/init", json={'id': model.id, 'name': '', 'data': model.to_b64()})
-        if response.ok:
-            return response.json()['error']
-        return response.text
 
-    def commit(self, model: Proposition, branch: str = 'main') -> bool:
+        if response.ok:
+            data = response.json()
+            if data['error']:
+                return None, data['error']
+            return key(data), None
+        return None, response.text
+
+    def commit(self, model: Proposition, branch: str = 'main') -> Tuple[Optional[str], Optional[str]]:
 
         """
             Commits `model` onto branch `branch`.
 
             Returns
             -------
-            bool
+            Tuple[Optional[str], Optional[str]]
+                Left side is the commit sha string, if it was successfully got. Else None.
+                Right side is an error string, if something went wrong. Else None.
         """
+        return self._extract_data(
+            requests.post(
+                f"{self.url}/{model.id}/{branch}/commit", 
+                json=model.to_b64()
+            ), 
+            key=lambda x: x['commit']['sha'],
+        )
+        
 
-        return requests.post(f"{self.url}/{model.id}/{branch}/commit", json=model.to_b64()).ok
-
-    def commit_assume(self, model_id: str, assumption: dict, branch: str = 'main', sha: str = None) -> Optional[Proposition]:
-
-        """
-            Assumes something on a model's commit (default latest) and returns the new assumed proposition.
-
-            Returns
-            -------
-            Optional[Proposition]
-        """
-        proposition = self.get_commit(model_id, branch=branch, sha=sha)
-        return proposition.assume(assumption)
-
-    def get_commit(self, model_id: str, branch: str = 'main', sha: str = None) -> Optional[Proposition]:
+    def checkout(self, sha: str) -> Optional[Proposition]:
 
         """
-            Gets latest commit for model (and branch). If `sha` is supplied, then that commit is got.
+            Checkout committed proposition by sha string.
 
             Returns
             -------
             Proposition
         """
-        if sha:
-            commit = requests.get(f"{self.url}/commit/{sha}").json()
-        else:
-            commit = requests.get(f"{self.url}/{model_id}/{branch}/latest").json()
-
-        return pg.from_b64(commit['commit']['data']) if commit['error'] is None else None
-
-    def create_branch(self, model_id: str, branch: str, on_branch: str = "main") -> bool:
-
-        """
-            Creates a new branch for model.
-
-            Returns
-            -------
-            bool
-        """
-        response = requests.post(f"{self.url}/{model_id}/{on_branch}/branch?name={branch}")
-        return response.ok and response.json()['error'] is None
-
+        return self._extract_data(
+            requests.get(f"{self.url}/commit/{sha}"), 
+            key=lambda commit: pg.from_b64(
+                commit['commit']['data'],
+            ),
+        )
