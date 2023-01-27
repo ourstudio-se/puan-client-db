8f82ecf03a6c1a4d6a8165e6b9785eab62c0a5e9
Rikard Olsson <rikard@thryselius.se>
2023-01-27 08:20:00 +0100

    Checkout_latest Proposition client Polyhedron client

--- puan_client_db/__init__.py
+++ puan_client_db/__init__.py
@@ -1,6 +1,11 @@
 import requests
 import puan.logic.plog as pg
+import pickle
+import gzip
+import base64
 
+from abc import abstractstaticmethod
+from puan.ndarray import ge_polyhedron
 from puan import Proposition
 from dataclasses import dataclass
 from typing import Optional, Tuple, Any
@@ -9,6 +14,33 @@ from typing import Optional, Tuple, Any
 class Client:
 
     url: str
+    default_main_branch: str = "main"
+
+    @abstractstaticmethod
+    def encode(model) -> str:
+
+        """
+            Encode model data to string.
+
+            Returns
+            -------
+                str
+        """
+
+        raise NotImplementedError("`Client` class is an abstract class. Implement `encode` and `decode`")
+
+    @abstractstaticmethod
+    def decode(model) -> str:
+
+        """
+            Encode model data to string.
+
+            Returns
+            -------
+                str
+        """
+
+        raise NotImplementedError("`Client` class is an abstract class. Implement `encode` and `decode`")
 
     @staticmethod
     def _extract_data(response, key) -> Tuple[Optional[Any], Optional[str]]:
@@ -30,7 +62,7 @@ class Client:
             return key(data), None
         return None, response.text
 
-    def commit(self, model: Proposition, branch: str = 'main') -> Tuple[Optional[str], Optional[str]]:
+    def commit(self, model: ge_polyhedron, id: str, branch: str = 'main') -> Tuple[Optional[str], Optional[str]]:
 
         """
             Commits `model` onto branch `branch`.
@@ -43,25 +75,87 @@ class Client:
         """
         return self._extract_data(
             requests.post(
-                f"{self.url}/{model.id}/{branch}/commit", 
-                json=model.to_b64()
+                f"{self.url}/{id}/{branch}/commit", 
+                json=self.encode(model),
             ), 
             key=lambda x: x['commit']['sha'],
         )
         
 
-    def checkout(self, sha: str) -> Optional[Proposition]:
+    def checkout(self, sha: str) -> Optional[Any]:
 
         """
             Checkout committed proposition by sha string.
 
             Returns
             -------
-            Proposition
+            Any
         """
         return self._extract_data(
             requests.get(f"{self.url}/commit/{sha}"), 
-            key=lambda commit: pg.from_b64(
+            key=lambda commit: self.decode(
                 commit['commit']['data'],
             ),
         )
+
+    def checkout_latest(self, id: str, branch: str = None) -> Optional[Any]:
+
+        """
+            Checkout the latest commit on model id and branch.
+
+            Returns
+            -------
+            Any
+        """
+        if branch is None:
+            branch = self.default_main_branch
+
+        return self._extract_data(
+            requests.get(f"{self.url}/{id}/{branch}/latest"),
+            key=lambda commit: self.decode(
+                commit['commit']['data'],
+            ),
+        )
+
+@dataclass
+class PropositionClient(Client):
+
+    @staticmethod
+    def encode(model) -> str:
+        return model.to_b64()
+
+    @staticmethod
+    def decode(data: str) -> Proposition:
+        return pg.from_b64(data)
+
+    
+
+@dataclass
+class PolyhedronClient(Client):
+
+    @staticmethod
+    def encode(model) -> str:
+        return base64.b64encode(
+            gzip.compress(
+                pickle.dumps(
+                    [model, model.variables, model.index, model.dtype],
+                    protocol=pickle.HIGHEST_PROTOCOL,
+                ),
+                mtime=0,
+            )
+        ).decode("utf8")
+
+    @staticmethod
+    def decode(data: str) -> Proposition:
+        try:
+            return ge_polyhedron(
+                *pickle.loads(
+                    gzip.decompress(
+                        base64.b64decode(
+                            data.encode()
+                        )
+                    )
+                )
+            )
+        except:
+            raise Exception("could not decompress and load polyhedron from string: version mismatch.")
